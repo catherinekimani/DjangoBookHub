bcdbb41e2d2d7f5cffa02123e1d03992f63d73ea
catherinekimani <catherinekimani882@gmail.com>
2023-12-09 23:28:33 +0300

    fix typo

--- BookHub/settings.py
+++ BookHub/settings.py
@@ -38,7 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'BookManager.app.BookmanagerConfig'
+    'BookManager.apps.BookmanagerConfig'
 ]
 
 MIDDLEWARE = [
