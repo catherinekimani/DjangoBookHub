928396934c3b22f1dfb42458dc57166822177e58
catherinekimani <catherinekimani882@gmail.com>
2023-12-17 21:32:43 +0300

    create url patterns for bookmanager views

--- BookManager/urls.py
+++ BookManager/urls.py
@@ -4,5 +4,9 @@ from . import views
 app_name = 'BookManager'
 
 urlpatterns = [
-	path('', views.home, name = "home"),
+	path('', views.home, name = 'home'),
+
+    path('signup', views.signup, name='signup'),
+    path('signin', views.signin, name='signin'),
+    path('signout', views.signout, name='signout')
 ]
