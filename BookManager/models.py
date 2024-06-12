f302c8178f42bec71ac2e420dcb338cbcce3a84b
catherinekimani <catherinekimani882@gmail.com>
2024-06-12 19:46:50 +0300

    Create custom user model

--- BookManager/models.py
+++ BookManager/models.py
@@ -1,7 +1,35 @@
 from django.db import models
-from django.contrib.auth.models import User
+
+from django.contrib.auth.models import AbstractUser
+from django.contrib.auth import get_user_model
+
+from django.conf import settings
 from cloudinary.models import CloudinaryField
+import secrets
+
+from .utils import generate_otp
+
+from django.utils import timezone
+from django.conf import settings
 # Create your models here.
+class CustomUser(AbstractUser):
+  email = models.EmailField(unique=True)
+  is_verified = models.BooleanField(default=False)
+  USERNAME_FIELD = ('email')
+  REQUIRED_FIELDS = ["username"]
+
+  def __str__(self):
+    return self.email
+
+class OtpToken(models.Model):
+		user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE, related_name='otp_tokens')
+		otp_code = models.CharField(max_length=6, default=generate_otp, editable=False)
+		created = models.DateTimeField(auto_now_add=True)
+		expires = models.DateTimeField(blank=True, null=True)
+
+		def __str__(self):
+			return self.user.username
+
 class Book(models.Model):
 	title = models.CharField(max_length = 100)
 	slug = models.SlugField(max_length=100, db_index=True)
